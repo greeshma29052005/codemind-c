7fdd78323042b8dd3ea7bc5fab8ea8af2586f3ab
greeshma29052005 <147472769+greeshma29052005@users.noreply.github.com>
2024-01-02 21:38:43 +0530

    #include<stdio.h> int main() {     int n,i,sum;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++){         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++){         if(i%2==1){             sum+=arr[i];         }     }     printf("%d",sum); }

---
+++ Sum_of_Odd_Positions.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,sum;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++){
+        if(i%2==1){
+            sum+=arr[i];
+        }
+    }
+    printf("%d",sum);
+}
