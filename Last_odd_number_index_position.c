cad662adebab918e1520a9f21c30606e2c3436de
greeshma29052005 <147472769+greeshma29052005@users.noreply.github.com>
2024-01-03 10:28:42 +0530

    #include<stdio.h> int main() {     int n,i,even=0,odd=0;     scanf("%d",&n);     int A[n];     for(i=0;i<n;i++){         scanf("%d",&A[i]);     }     for(i=0;i<n;i++){         if(i%2==1);     }     printf("%d",i-1); }

---
+++ Last_odd_number_index_position.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,even=0,odd=0;
+    scanf("%d",&n);
+    int A[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&A[i]);
+    }
+    for(i=0;i<n;i++){
+        if(i%2==1);
+    }
+    printf("%d",i-1);
+}
