6e2160ffcd75235645364cb7f23e40f0df309c0e
greeshma29052005 <147472769+greeshma29052005@users.noreply.github.com>
2024-03-13 11:42:36 +0530

    #include<stdio.h> #include<string.h> int main() {     char s[1000];     scanf("%[^ ]s",s);     int i,sum=0,cnt=0;     for(i=0;s[i]!=NULL;i++){         if(s[i]>='0' && s[i]<='9'){             sum+=(s[i]-'0');         }     }     printf("%d",sum); }

---
+++ _Sum_of_all_numbers_present_in_the_String.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[1000];
+    scanf("%[^
+]s",s);
+    int i,sum=0,cnt=0;
+    for(i=0;s[i]!=NULL;i++){
+        if(s[i]>='0' && s[i]<='9'){
+            sum+=(s[i]-'0');
+        }
+    }
+    printf("%d",sum);
+}
